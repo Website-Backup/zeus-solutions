37e3b6c16d7c482127b62af240c3b1cb7bb884d1
sivasathyaseeelan <dnsiva.sathyaseelan.chy21@iitbhu.ac.in>
2023-06-12 20:07:49 +0530

    api key secret

--- src/config/firebase.jsx
+++ src/config/firebase.jsx
@@ -12,7 +12,7 @@ const firebaseConfig = {
   authDomain: process.env.REACT_APP_AUTHDOMAIN,
   databaseURL: process.env.REACT_APP_DATABASEURL,
   projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.REACT_APP_STORAGEBUCKET,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
   appId: process.env.REACT_APP_APPID,
   measurementId: process.env.REACT_APP_MEASUREMENTID
