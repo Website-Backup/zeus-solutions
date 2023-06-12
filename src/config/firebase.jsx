2b81227d2a123ea65a53a6d3dc6da571b960d214
sivasathyaseeelan <dnsiva.sathyaseelan.chy21@iitbhu.ac.in>
2023-06-12 20:57:40 +0530

    done

--- src/config/firebase.jsx
+++ src/config/firebase.jsx
@@ -8,14 +8,14 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASEURL,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_APPID,
-  measurementId: process.env.REACT_APP_MEASUREMENTID
+  apiKey: "AIzaSyDEkE4DGI15yNnlkozo_A2ZOLOUwzeJphM",
+  authDomain: "zeus-solutions.firebaseapp.com",
+  databaseURL: "https://zeus-solutions-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "zeus-solutions",
+  storageBucket: "zeus-solutions.appspot.com",
+  messagingSenderId: "291720212725",
+  appId: "1:291720212725:web:edfb237f4c086889df55bc",
+  measurementId: "G-QS9YCMBJNE"
 };
 
 // Initialize Firebase
